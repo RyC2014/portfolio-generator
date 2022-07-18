0a2691b21450043f425b8a31887b95086d00e498
RyC2014 <rclingler1986@gmail.com>
2022-07-18 11:08:03 -0400

    adding arrays

--- app.js
+++ app.js
@@ -13,8 +13,38 @@ console.log(profileDataArgs);
 
 
 
-const printProfileData = (profileDataArr) => {
-    console.log(profileDataArr);
-  };
+// const printProfileData = (profileDataArr) => {
+//     console.log(profileDataArr);
+//   };
   
-  printProfileData(profileDataArgs);
+//   printProfileData(profileDataArgs);
+
+// Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++) {
+//       console.log(profileDataArr[i]);
+//     }
+//   };
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+
+    }
+  
+
+
+
+
+
+
+
+
+
