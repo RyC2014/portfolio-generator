ac49e89a4834a02343092e3096b9441a65c14ed1
RyC2014 <rclingler1986@gmail.com>
2022-07-18 10:55:46 -0400

    editing node.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello Node!');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const printProfileData = (profileDataArr) => {
+    console.log(profileDataArr);
+  };
+  
+  printProfileData(profileDataArgs);
